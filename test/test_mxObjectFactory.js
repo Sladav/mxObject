c529e0702ff69047efdaa8e39dc7431a9b38ba82
Steve Ladavich <steve.ladavich@gmail.com>
2016-10-08 23:16:13 -0400

    Modified test structure to use separate files

---
+++ test/test_mxObjectFactory.js
@@ -0,0 +1,19 @@
+let expect = require('chai').expect;
+let mxObject = require('../mxObject');
+
+describe('mxObject Factory', function(){
+  describe('input validation',function(){
+    it('should return empty mxObject by default', function(){
+      let mxObj = mxObject();
+      expect(mxObj).to.be.an('object')
+      expect(mxObj.__type__).to.equal('mxObject')
+      expect(mxObj).to.be.empty
+      expect(mxObj.__mixins__).to.be.an('array')
+      expect(mxObj.__mixins__).to.be.empty
+      expect(mxObj.__extensions__).to.be.an('array')
+      expect(mxObj.__extensions__).to.have.length(1)
+      expect(mxObj.__extensions__[0]).to.be.an('object')
+      expect(mxObj.__extensions__[0]).to.be.empty
+    })
+  })
+})
