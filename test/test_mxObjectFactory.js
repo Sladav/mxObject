a66ac8ea7ea58a0ba0d893ab064d98a14b6af787
Steve Ladavich <steve.ladavich@gmail.com>
2016-11-04 21:39:40 -0400

    Modified test to fit change in initializing

--- test/test_mxObjectFactory.js
+++ test/test_mxObjectFactory.js
@@ -11,9 +11,7 @@ describe('mxObject Factory', function(){
       expect(mxObj.__mixins__).to.be.an('array')
       expect(mxObj.__mixins__).to.be.empty
       expect(mxObj.__extensions__).to.be.an('array')
-      expect(mxObj.__extensions__).to.have.length(1)
-      expect(mxObj.__extensions__[0]).to.be.an('object')
-      expect(mxObj.__extensions__[0]).to.be.empty
+      expect(mxObj.__extensions__).to.have.length(0)
     })
   })
 })
